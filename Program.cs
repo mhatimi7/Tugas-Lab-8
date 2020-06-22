d219b1647ea04973b7ad9e81c545a5611c4299f6
M Hatimi Al Jaudi <61742580+mhatimi7@users.noreply.github.com>
2020-06-22 21:01:12 +0700

    Tugas Lab 8

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TugasLab8.ClassAnak;
+using TugasLab8.ClassInduk;
+
+namespace TugasLab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tugas Lab 8 - Inheritance, Polymorphsim, Abstraction & Collection";
+
+            // Membuat Objek Untuk Semua Karyawan
+
+            KaryawanTetap karyawanTetap = new KaryawanTetap();
+            karyawanTetap.NIK = "123-234-345";
+            karyawanTetap.Nama = "Michael Suyama";
+            karyawanTetap.GajiBulanan = 4000000;
+
+            KaryawanHarian karyawanHarian = new KaryawanHarian();
+            karyawanHarian.NIK = "123-321-346";
+            karyawanHarian.Nama = "Laura Callahan";
+            karyawanHarian.UpahPerJam = 13000;
+            karyawanHarian.JumlahJamKerja = 8;
+
+            Sales sales = new Sales();
+            sales.NIK = "123-333-347";
+            sales.Nama = "Andrew Fuller";
+            sales.JumlahPenjualan = 30;
+            sales.Komisi = 45000;
+
+            // Objek Class Collection
+            List<Karyawan> listKaryawan = new List<Karyawan>();
+
+            listKaryawan.Add(karyawanTetap);
+            listKaryawan.Add(karyawanHarian);
+            listKaryawan.Add(sales);
+
+            int noUrut = 1;
+
+            foreach (Karyawan karyawan in listKaryawan)
+            {
+                Console.WriteLine("{0}. NIK: {1}, Nama: {2}, Gaji: {3}", noUrut, karyawan.NIK, karyawan.Nama, karyawan.Gaji());
+
+                noUrut++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
